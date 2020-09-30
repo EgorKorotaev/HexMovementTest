21d7051e09005c45955f949cdfd447c7336ed711
Faunus_C <FaunusCarmel@yandex.ru>
2020-09-30 22:38:46 +0300

    Create Map generator

--- Assets/code/Map.cs
+++ Assets/code/Map.cs
@@ -1,3 +1,19 @@
-﻿public class Map
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map
 {
+    public Map(ref GameObject[,] grid, int shiftR, int shiftQ)
+    {
+        this.grid = grid;
+        this.shiftR = shiftR;
+        this.shiftQ = shiftQ;
+    }
+    
+    public GameObject[,] grid;
+    private int shiftR;
+    private int shiftQ;
+    
 }
+
--- Assets/code/MapGenerator.cs
+++ Assets/code/MapGenerator.cs
@@ -3,13 +3,31 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    public void GenerateMap()
+    public GameObject haxObjectPrefab;
+    private const float PositionShiftX = 1.5f;
+    private const float PositionShiftZ = 0.8660254f; // sqrt(3) / 2
+    
+    public Map GenerateMap(int rOrigin, int qOrigin, int width, int height)
     {
-        Debug.Log("Создали карту");
+        GameObject[,] array = new GameObject[width,height];
+
+        for (int r = 0; r < width; ++r)
+        {
+            for (int q = 0; q < height; ++q)
+            {
+                var R = (r + rOrigin);
+                var Q = (q + qOrigin);
+                var position = new Vector3(R * PositionShiftX, 0, R * PositionShiftZ + Q * PositionShiftZ * 2);
+                GameObject newHex = Instantiate(haxObjectPrefab, position, Quaternion.identity);
+                array[r, q] = newHex;
+            } 
+        }
+
+        return new Map(ref array, rOrigin, qOrigin);
     }
     
     private void Start()
     {
-        EventManager.StartListening(Events.OnGenerateMapEvent, GenerateMap);
+        GenerateMap(0, 0, 20, 20);
     }
 }
